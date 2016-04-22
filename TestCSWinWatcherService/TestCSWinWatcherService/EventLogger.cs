2cda7758800966d922cd7d39d3ba3bac1dc9c315
Monica Nicolae <m.nicolae@netrom.local>
2016-04-22 14:20:07 +0300

    Copy from drop folder to one subfolder

---
+++ TestCSWinWatcherService/TestCSWinWatcherService/EventLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TestCSWinWatcherService
+{
+     public class EventLogger
+    {
+        private string eventViewerSource = "TestCSWinService";
+        private string eventViewerLog = "Errors";
+        
+
+         public void InitLog()
+         {
+            
+             // init the log
+
+             if (!EventLog.SourceExists(eventViewerSource))
+             {
+                 EventLog.CreateEventSource(eventViewerSource, eventViewerLog);
+             }
+             
+              
+         }
+
+         public void _writeToLog(Exception ex)
+         {
+             EventLog.WriteEntry(eventViewerSource, ex.Message + ex.StackTrace);
+         }
+
+    }
+}
---
+++ TestCSWinWatcherService/TestCSWinWatcherService/Service1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestCSWinWatcherService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private FileWatcherManager _fileWatcherManager;
+       
+
+        public Service1()
+        {
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+            _fileWatcherManager = new FileWatcherManager();
+            _fileWatcherManager.Init();
+            
+            
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
